25926703740b047adc08b41a7460322184450b50
AnastsiaRom <51119682+AnastsiaRom@users.noreply.github.com>
2022-04-15 15:49:29 +0500

    add

---
+++ convertor/app/pages/PageLength.vue
@@ -0,0 +1,44 @@
+<template>
+  <Page>
+    <ActionBar title="длина" />
+    <StackLayout verticalAlignment="top">
+      <convertor :ToDefault="ToDefault" :ToNeed="ToNeed" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+import Convertor from "@/components/Convertor.vue";
+
+export default {
+  data() {
+    return {
+      ToDefault: {
+        Сантиметры: function (val) {
+          return val;
+        },
+        Метры: function (val) {
+          return val * 100;
+        },
+        мили: function (val) {
+          return val / 0.0000062137;
+        },
+      },
+      ToNeed: {
+        Сантиметры: function (val) {
+          return val;
+        },
+        Метры: function (val) {
+          return val / 100;
+        },
+        мили: function (val) {
+          return val * 0.0000062137;
+        },
+      },
+    };
+  },
+  components: {
+    Convertor,
+  },
+};
+</script>
